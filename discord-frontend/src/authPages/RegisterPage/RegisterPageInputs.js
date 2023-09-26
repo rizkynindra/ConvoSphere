d2dcbcaf130d0bc00adef63efa60b8d98b36c048
rizky nindra <rizkynindra.sukma@bpjsketenagakerjaan.go.id>
2023-09-26 08:47:09 +0700

    Update RegisterPageInputs.js

--- discord-frontend/src/authPages/RegisterPage/RegisterPageInputs.js
+++ discord-frontend/src/authPages/RegisterPage/RegisterPageInputs.js
@@ -2,7 +2,7 @@ import React from "react";
 import InputWithLabel from "../../shared/components/InputWithLabel";
 
 const RegisterPageInputs = (props) => {
-  const { mail, setMail, username, setUsername, password, setPassword } = props;
+  const { mail, setMail, username, setUsername, password, setPassword, phonenum, setPhonenum } = props;
 
   return (
     <>
@@ -26,6 +26,13 @@ const RegisterPageInputs = (props) => {
         label="Password"
         type="password"
         placeholder="Enter password"
+      />
+      <InputWithLabel
+        value={phonenum}
+        setValue={setPhonenum}
+        label="Phone Number"
+        type="text"
+        placeholder="Enter your phone number. Eg: 08123456789"
       />
     </>
   );
